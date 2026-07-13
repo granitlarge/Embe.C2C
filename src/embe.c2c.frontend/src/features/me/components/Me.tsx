fef9908b79bc2b5a780359fc610d6d019de5e2cd
Miro Bakovic <bakovicmiro@outlook.com>
2026-07-13 19:09:09 +0200

    add AlertDialog on clearing of location

--- src/embe.c2c.frontend/src/features/me/components/Me.tsx
+++ src/embe.c2c.frontend/src/features/me/components/Me.tsx
@@ -12,6 +12,7 @@ import { Gender } from "@/src/shared/types/domain/value-objects";
 import Modal from "@/src/shared/components/modal/Modal";
 import Profile from "@/src/shared/components/user/Profile";
 import { calculateAge } from "@/src/shared/time";
+import AlertDialog from "@/src/shared/components/infos/Warning";
 
 export type MeProps = {
     className?: string,
@@ -130,56 +131,72 @@ export default function Me({ className, user }: MeProps) {
                 }));
             }} />
             <div className="flex flex-row gap-3 justify-end">
-                <Button onClick={onSave} intent="save">save</Button>
+                {(serverSideBasicFormData.location === undefined || clientSideBasicFormData.location !== undefined) && <Button onClick={onSave} intent="save">save</Button>}
+                {
+                    (serverSideBasicFormData.location !== undefined) && (clientSideBasicFormData.location === undefined) &&
+                    <AlertDialog
+                        title="are you sure?"
+                        description='clearing your location will disable the maximum distance filter on all of your search profiles.'
+                        onCancel={() => {
+                            setClientSideBasicFormData(prev => ({
+                                ...prev,
+                                location: serverSideBasicFormData.location
+                            }))
+                        }}
+                        onConfirm={onSave}
+                    >
+                        <Button intent="save">save</Button>
+                    </AlertDialog>
+                }
                 <Button onClick={onPreview} intent="preview">preview</Button>
                 <Button onClick={onCancel} intent="cancel">cancel</Button>
             </div>
             {
                 showPreview && <Modal className="surface-secondary" hidden={false} closed={() => setShowPreview(false)} header="preview">
-                <Profile
-                    candidate={{
-                        id: user.data?.id!,
-                        bio: clientSideBasicFormData.bio,
-                        alias: clientSideBasicFormData.alias,
-                        birthDate: clientSideBasicFormData.birthDate,
-                        gender: clientSideBasicFormData.gender,
-                        datingPreferences: user.data?.datingPreferences,
-                        location: clientSideBasicFormData.location,
-                        distanceKmToQueryingUser: user.data?.distanceKmToQueryingUser || 0,
-                        age: clientSideBasicFormData.birthDate ? calculateAge(clientSideBasicFormData.birthDate) : undefined,
-                        createdAt: user.data?.createdAt,
-                        updatedAt: user.data?.updatedAt,
-                        email: user.data?.email,
-                        profilePicture: {
-                            id: "",
-                            ownerUserId: "",
-                            imageDetails: {
-                                url: clientSideBasicFormData.images?.[0]?.url ?? "",
-                                mimeType: clientSideBasicFormData.images?.[0]?.mimeType ?? "",
-                                order: 0,
-                                name: "",
+                    <Profile
+                        candidate={{
+                            id: user.data?.id!,
+                            bio: clientSideBasicFormData.bio,
+                            alias: clientSideBasicFormData.alias,
+                            birthDate: clientSideBasicFormData.birthDate,
+                            gender: clientSideBasicFormData.gender,
+                            datingPreferences: user.data?.datingPreferences,
+                            location: clientSideBasicFormData.location,
+                            distanceKmToQueryingUser: user.data?.distanceKmToQueryingUser || 0,
+                            age: clientSideBasicFormData.birthDate ? calculateAge(clientSideBasicFormData.birthDate) : undefined,
+                            createdAt: user.data?.createdAt,
+                            updatedAt: user.data?.updatedAt,
+                            email: user.data?.email,
+                            profilePicture: {
+                                id: "",
+                                ownerUserId: "",
+                                imageDetails: {
+                                    url: clientSideBasicFormData.images?.[0]?.url ?? "",
+                                    mimeType: clientSideBasicFormData.images?.[0]?.mimeType ?? "",
+                                    order: 0,
+                                    name: "",
+                                },
+                                markedForDeletionAt: null,
+                                deletedAt: null,
+                                createdAt: ""
                             },
-                            markedForDeletionAt: null,
-                            deletedAt: null,
-                            createdAt: ""
-                        },
-                        images: clientSideBasicFormData.images?.map((image, index) => ({
-                            id: image.id ?? "",
-                            ownerUserId: "",
-                            createdAt: user.data?.createdAt ?? "",
-                            updatedAt: new Date().toISOString(),
-                            imageDetails: {
-                                url: image.url,
-                                mimeType: image.mimeType,
-                                order: index,
-                                name: "",
-                            },
-                            markedForDeletionAt: null,
-                            deletedAt: null
-                        })) ?? []
-                    }}
-                />
-            </Modal>
+                            images: clientSideBasicFormData.images?.map((image, index) => ({
+                                id: image.id ?? "",
+                                ownerUserId: "",
+                                createdAt: user.data?.createdAt ?? "",
+                                updatedAt: new Date().toISOString(),
+                                imageDetails: {
+                                    url: image.url,
+                                    mimeType: image.mimeType,
+                                    order: index,
+                                    name: "",
+                                },
+                                markedForDeletionAt: null,
+                                deletedAt: null
+                            })) ?? []
+                        }}
+                    />
+                </Modal>
             }
         </Surface>
 
---
+++ src/embe.c2c.frontend/src/shared/components/infos/Warning.tsx
@@ -0,0 +1,39 @@
+import * as RadixAlertDialog from "@radix-ui/react-alert-dialog"
+import Button from "../buttons/Button";
+export type AlertDialogProps = {
+    children: React.ReactNode;
+    title: string;
+    description: string;
+    onConfirm: () => void | Promise<void>;
+    onCancel: () => void | Promise<void>;
+}
+export default function AlertDialog({ children, title, description, onConfirm, onCancel }: AlertDialogProps) {
+    return (
+        <RadixAlertDialog.Root>
+            <RadixAlertDialog.Trigger asChild>
+                {children}
+            </RadixAlertDialog.Trigger>
+            <RadixAlertDialog.Portal>
+                <RadixAlertDialog.Overlay className="bg-white fixed inset-0" />
+                <RadixAlertDialog.Content className="flex flex-col gap-3 surface-secondary fixed top-1/2 left-1/2 -translate-1/2 w-full p-3">
+                    <div className="flex flex-col gap-0">
+                        <RadixAlertDialog.Title className="mx-auto">
+                            {title}
+                        </RadixAlertDialog.Title>
+                        <RadixAlertDialog.Description className="mx-auto  text-center text-(--secondary-fc) text-(length:--secondary-fs)">
+                            {description}
+                        </RadixAlertDialog.Description>
+                    </div>
+                    <div className="flex gap-2">
+                        <RadixAlertDialog.Action asChild>
+                            <Button intent="save" onClick={onConfirm}>continue</Button>
+                        </RadixAlertDialog.Action>
+                        <RadixAlertDialog.Cancel asChild>
+                            <Button intent="cancel" onClick={onCancel}>cancel</Button>
+                        </RadixAlertDialog.Cancel>
+                    </div>
+                </RadixAlertDialog.Content>
+            </RadixAlertDialog.Portal>
+        </RadixAlertDialog.Root>
+    )
+}
